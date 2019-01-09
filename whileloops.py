24dbac9f2ad732f7aad4b02ac3d70a3e9a74e663
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-01-09 04:44:22 -0700

    Add files via upload

---
+++ whileloops.py
@@ -0,0 +1,61 @@
+'''
+Resources :
+
+https://www.python-course.eu/python3_loops.php
+
+
+
+
+'''
+
+import random
+x= 1
+while x < 10:
+    print(x)
+    x += 1
+x = 1
+y = 1
+while x < 10:
+    while y < 5:
+        print ( x,y,' ', end='')
+        y = y +1
+    print()
+    x = x + 1
+    y = 1
+
+# Guessing a number
+
+
+
+n = 20
+to_be_guessed = int(n * random.random()) + 1
+guess = 0
+while guess != to_be_guessed:
+    guess = int(input("New number: "))
+    if guess > 0:
+        if guess > to_be_guessed:
+            print("Number too large")
+        elif guess < to_be_guessed:
+            print("Number too small")
+    else:
+        print("Sorry that you're giving up!")
+        break
+else:
+    print("Congratulation. You made it!")
+
+#
+# Fibonacci Numbers using reassigning variables
+
+
+x = 1
+y = 1
+
+cont = 'c'
+while cont =='c':
+    print("A fibon number: ", x+y)
+    temp = x
+
+    x = y
+    y = temp + y
+
+    cont = input('if continuu press c, else press any other key')
