43058ca910b385de9f58edb3e153f06b80e3d092
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-01-15 14:59:18 -0700

    Add files via upload

---
+++ mydict.py
@@ -0,0 +1,26 @@
+
+
+dict = {'Name': 'Nameless One', 'Strength': 'Very powerful skills' , 'Class' : 'warrior'}
+
+print("dict['Name']", dict['Name'])
+print(dict.keys())
+print(dict.values())
+
+# Translate a text using a dictionary
+
+text = "This is a simple text to check whether the Name as a character" \
+       " having good Strength could be replacing a Class fighter"
+print (text)
+text_list = text.split(' ')
+print( text_list)
+for i in range(len( text_list)):
+    for key in dict.keys():
+        if text_list[i] == key:
+            text_list.remove(text_list[i])
+            text_list.insert( i, dict[key] )
+
+
+new_text = ''
+for word in text_list:
+    new_text = new_text +  ' ' + word
+print(new_text)
