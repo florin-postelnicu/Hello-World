eccd7129f7aef6c54364799da3b5bb16076d8533
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-01-18 04:58:08 -0700

    Update florinlib.py

--- florinlib.py
+++ florinlib.py
@@ -89,3 +89,17 @@ def translate_text(text, mydict):
 
 
 print(translate_text(text1, dict1))
+# a second translate function
+def translate(text,translation_map):
+    #use raw input and split the sentence into a list of words
+    input_list = text.split()
+    output_list = []
+
+    #iterate the input words and append translation
+    #(or word if no translation) to the output
+    for word in input_list:
+        translation = translation_map.get(word)
+        output_list.append(translation if translation else word)
+
+    #convert output list back to string
+    return ' '.join(output_list)
