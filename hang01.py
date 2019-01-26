9b9df4b21f1f68edde015ae772aa1bd467307920
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-01-25 22:14:37 -0700

    Add files via upload

--- hang01.py
+++ hang01.py
@@ -2,21 +2,14 @@
 word = 'hangman'
 secret_word = list(word)
 guess_word = ['*' for i in range(len(secret_word))]
-# for i in range(len(secret_word)):
-#     guess_word.append('*')
-
 
 penalty = 0
 list_letters = []
 
-
 while "*" in guess_word and penalty < 7:
     guess = input('Enter your guess letter')
 
-    if guess not in secret_word:
-        penalty += 1
-
-    if guess  in list_letters:
+    if guess not in secret_word or guess in list_letters:
         penalty += 1
 
     else:
