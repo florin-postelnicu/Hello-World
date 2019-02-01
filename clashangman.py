970869be3800f971d5d058de18e430215127eab4
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-02-01 11:25:08 -0700

    Add files via upload

---
+++ clashangman.py
@@ -0,0 +1,58 @@
+
+class Player:
+    def __init__(self, name , battles , victories  ):
+        self.name = name
+        self.battles = battles
+        self.victories = victories
+
+    def greetings(self):
+        self.name = input("Enter your name warrior! \n")
+        print(self.name)
+        history = {'Archy': (10,3), 'Kareandru': (100, 75)}
+        #  Check if the name is in the history.names
+        # Use get_warrior_infos()
+        if self.name in history.keys():
+
+            # Yes call the Getters
+            self.battles = history[self.name][0]
+            self.victories = history[self.name][1]
+            Player.print_stats(self)
+            # self.get_warrior_infos()
+        else :
+
+            # No call the Setters
+            Player.set_waorrior_infos(self)
+            history[self.name] = (self.battles, self.victories)
+            Player.print_stats(self)
+        print(history)
+
+    # Define Getters
+
+    def get_warrior_infos(self):
+        # Read the file "History", and check if the name of the player it is
+        # already in
+        pass
+
+    # Define Setters For newbies
+    def set_waorrior_infos(self):
+        print("What's your story {}  warrior? \n".format(self.name))
+        self.battles = int(input("Enter your skirmishes big warrior \n"))
+        self.victories = int(input("Enter the number of annihilated enemies \n"))
+
+    def print_stats(self):
+        print("Warrior ", self.name)
+        print("Lots of battles ", self.battles)
+        print("Lots of winning scars ", self.victories)
+    def all_warriors(self):
+        pass
+
+
+
+Player.greetings(Player)
+# player1 = Player("Moody", 35, 12)
+# Player.print_stats(player1)
+# p2 = Player
+# Player.set_waorrior_infos(p2)
+# Player.print_stats(p2)
+
+
