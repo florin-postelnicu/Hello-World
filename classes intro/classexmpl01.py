6fb4cc23d69576cf7e6b551830ecd61d34c99dba
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-02-08 06:14:56 -0700

    Add files via upload

--- classes intro/classexmpl01.py
+++ classes intro/classexmpl01.py
@@ -18,15 +18,17 @@ class CashRegister:
         self.price += price
 
     def display_record(self):
-        return print('total price = ', self.price, 'Number of items = ', self.items)
+        return print('total price  without taxes = ', self.price, 'Number of items = ', self.items)
 
     def price_with_taxes(self):
-        pass
+        self.price = 1.2*self.price
+        return  print('Total price with taxes ', self.price)
 
     def discounts(self):
         pass
 
     def clear_register(self):
+        CashRegister.price_with_taxes(self)
         self.items = 0
         self.price = 0
 
@@ -37,8 +39,12 @@ def main():
     reg1.update_register(29.25)
     reg1.update_register(12.75)
     reg1.display_record()
+
     reg1.clear_register()
     reg1.display_record()
+    # reg1.update_register(40)
+    # reg1.display_record()
+    # reg1.price_with_taxes()
 
 
 if __name__ == '__main__':
